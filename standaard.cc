8bc827b54eb4e0232484894b18242faf3ccba692
Bart-Aaldering <79908741+Bart-Aaldering@users.noreply.github.com>
2021-05-21 17:04:55 +0200

    Update standaard.cc

--- standaard.cc
+++ standaard.cc
@@ -9,54 +9,60 @@ using namespace std;
 //*************************************************************************
 
 bool integerInBereik (const char *variabele, int waarde,
-                      int minWaarde, int maxWaarde)
+					int minWaarde, int maxWaarde)
 {
-  if (waarde>=minWaarde && waarde<=maxWaarde)
-    return true;
-  else
-  { cout << variabele << "=" << waarde << ", maar moet in [" << minWaarde
-         << "," << maxWaarde << "] liggen." << endl;
-    return false;
-  }
-
+	if (waarde>=minWaarde && waarde<=maxWaarde)
+		return true;
+	else
+	{ cout << variabele << "=" << waarde << ", maar moet in [" << minWaarde
+			<< "," << maxWaarde << "] liggen." << endl;
+		return false;
+	}
 }  // integerInBereik
 
 //*************************************************************************
 
 int randomGetal (int min, int max)
-{ int bereik,
-      r;
+{
+	int bereik,
+	r;
 
-  bereik = max - min + 1;
+	bereik = max - min + 1;
 
-  r = ((rand())%bereik) + min;
-  return r;
-
+	r = ((rand())%bereik) + min;
+	return r;
 }  // randomGetal
 
 //****************************************************************************
 
 double randomDouble (double min, double max)
-{ int bereik,
-      r;
-  double fr;
+{
+	int bereik, r;
+	double fr;
 
-  bereik = (int)(100*(max - min) + 1);
-  
-  r = (rand())%bereik;  // 0 <= r <= 100*(max-min)
-  fr = min + 0.01*r;
-  
-  return fr;
+	bereik = (int)(100*(max - min) + 1);
 
+	r = (rand())%bereik;  // 0 <= r <= 100*(max-min)
+	fr = min + 0.01*r;
+	return fr;
 }  // randomDouble
 
 //****************************************************************************
 
 void schrijfDouble (double getal, int precisie, bool metEndln)
 {
-  cout << setiosflags (ios::fixed) << setprecision (precisie) << getal;
-  if (metEndln)
-    cout << endl;
-
+	cout << setiosflags (ios::fixed) << setprecision (precisie) << getal;
+	if (metEndln)
+		cout << endl;
 }  // schrijfDouble
 
+//****************************************************************************
+
+int macht(int grondtal, int exponent)
+{
+	int macht = 1;
+	for (int i = 0; i < exponent; i++) {
+		macht *= grondtal;
+	}
+	return macht;
+}
