e6b5a5700da8386f983e8d3d8018c2c2709d695a
LexJanssens <s2989344@vuw.leidenuniv.nl>
2021-05-26 15:20:17 +0200

    Update constantes.h

--- constantes.h
+++ constantes.h
@@ -1,10 +1,16 @@
 // Definitie van enkele constantes
-
+// constantes.h
+// Geschreven door : Bart Aaldering (s2969866) en Lex Janssens (s2989344)
+// Compiler: GNU GCC Compiler
+// Beschrijving:
+/*
+	Enkele constantes gebruikt in het programma
+*/
 #ifndef ConstantesHVar  // voorkom dat dit bestand meerdere keren
 #define ConstantesHVar  // ge-include wordt
 
-const int MaxTw = 100;
-const int MaxN = 8;
+const int MaxTw = 100;     // Maximaal aantal dagen
+const int MaxN = 8;        // Maximaal aantal beurzen
 const int MaxAs = 256;     // 2^{MaxN}, het maximum aantal bitstrings a
 
 #endif
